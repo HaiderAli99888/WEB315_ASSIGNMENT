176344aee0494b58ddd860b6f75a245167d38212
HaiderAli <w0828832@myscc.ca>
2023-02-09 19:10:02 -0500

    Created Data model

---
+++ PizzaApp/Models/Pizza.cs
@@ -0,0 +1,19 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesPizza.Models
+{
+    public class Pizza
+    {
+        public int ID { get; set; }
+        public string PizzaName { get; set; }
+    
+
+        [DataType(DataType.Date)]
+        public DateTime VisitDate { get; set; }
+        public string Category { get; set; }
+        public string PizzaSize { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
