a116d0d1d2f025aa76d50a2f6fc10a5526e6bc0e
Haider <w0828832@myscc.ca>
2023-02-20 15:33:33 -0500

    added validation

--- PizzaApp/Models/Pizza.cs
+++ PizzaApp/Models/Pizza.cs
@@ -1,20 +1,34 @@
 using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RazorPagesPizza.Models
 {
     public class Pizza
     {
         public int ID { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string PizzaName { get; set; }
     
 
         [DataType(DataType.Date)]
         public DateTime VisitDate { get; set; }
         public string Category { get; set; }
+
+        [Required]
+        [StringLength(20, MinimumLength = 3)]
         public string PizzaSize { get; set; }
+
+        [Range(1, 2000)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(4, 3)")]
         public decimal Price { get; set; }
 
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(10, MinimumLength = 3)]
         public string Topping { get; set; }
         public string Rating { get; set; }
         public int Discount { get; set; }
